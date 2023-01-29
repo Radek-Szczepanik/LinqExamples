cfdfcb352f0218f040338a7b12c3cc928411f9c3
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2023-01-29 11:32:04 +0100

    Add linq last

---
+++ LinqLast/Program.cs
@@ -0,0 +1,35 @@
+﻿// Last - get last element from collection
+
+var numbers = new List<int> { 5, 2, 1, 6, 3, 1, 4, 5, 6, 1, 7, 8, 9, 10, 1 };
+
+// Get first element
+var lastElement = numbers.Last();
+
+Console.WriteLine("Last element:");
+Console.WriteLine(lastElement);
+
+Console.WriteLine();
+
+// Get last element equals 10
+var lastElementEqualsTen = numbers.Last(x => x == 10);
+
+Console.WriteLine("Last element equals ten:");
+Console.WriteLine(lastElementEqualsTen);
+
+Console.WriteLine();
+
+// Get last element equals 11 - there is not in collection (will return exception)
+var lastElementEqualsEleven = numbers.Last(x => x == 11);
+
+Console.WriteLine("Last element equals eleven:");
+Console.WriteLine(lastElementEqualsEleven);
+
+Console.WriteLine();
+
+// Get last element equals 12 - there is not in collection (use LastOrDefault will return default type)
+var lastElementEqualsTwelve = numbers.LastOrDefault(x => x == 12);
+
+Console.WriteLine("Last element equals twelve:");
+Console.WriteLine(lastElementEqualsTwelve);
+
+Console.WriteLine();
--- LinqSingle/Program.cs
+++ LinqSingle/Program.cs
@@ -5,7 +5,7 @@ var numbers = new List<int> { 5, 2, 1, 6, 3, 1, 4, 5, 6, 1, 7, 8, 9, 5, 10, 1 };
 // Get single element - number 2 occurs once in collection
 var singleElementTwo = numbers.Single(x => x == 2);
 
-Console.WriteLine("Single element two:");
+Console.WriteLine("Single element equals two:");
 Console.WriteLine(singleElementTwo);
 
 Console.WriteLine();
@@ -13,7 +13,7 @@ Console.WriteLine();
 // Get single element - number 1 occurs a few times in collection (will return exception)
 var singleElementOne = numbers.Single(x => x == 1);
 
-Console.WriteLine("Single element one:");
+Console.WriteLine("Single element equals one:");
 Console.WriteLine(singleElementOne);
 
 Console.WriteLine();
@@ -21,7 +21,7 @@ Console.WriteLine();
 // Get single element - number 5 occurs a few times in collection (use SingleOrDefault will return exception)
 var singleElementFive = numbers.SingleOrDefault(x => x == 5);
 
-Console.WriteLine("Single element five:");
+Console.WriteLine("Single element equals five:");
 Console.WriteLine(singleElementFive);
 
 Console.WriteLine();
@@ -29,7 +29,7 @@ Console.WriteLine();
 // Get single element - number 11 is not in collection (will return exception)
 var singleElementEleven = numbers.Single(x => x == 11);
 
-Console.WriteLine("Single element eleven:");
+Console.WriteLine("Single element equals eleven:");
 Console.WriteLine(singleElementEleven);
 
 Console.WriteLine();
@@ -37,7 +37,7 @@ Console.WriteLine();
 // Get single element - number 12 is not in collection (use SingleOrDefault will return default type)
 var singleElementTwelve = numbers.SingleOrDefault(x => x == 12);
 
-Console.WriteLine("Single element twelve:");
+Console.WriteLine("Single element equals twelve:");
 Console.WriteLine(singleElementTwelve);
 
 Console.WriteLine();
