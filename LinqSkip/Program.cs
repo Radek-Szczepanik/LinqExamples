355ae35fd672993846dbf2e1b8f95f6039c144c1
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2023-01-30 15:12:27 +0100

    Add linq skip

---
+++ LinqSkip/Program.cs
@@ -0,0 +1,14 @@
+﻿// Skip - skip the given amount of elements
+
+var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+// Skip first 5 elements
+var skipFiveElements = numbers.Skip(5);
+Console.WriteLine("Skip 5 elements:");
+Console.WriteLine(string.Join(", ", skipFiveElements));
+Console.WriteLine();
+
+// Skip first 11 elements - will return empty list because list of numbers contains only 10 elements
+var skipElevenElements = numbers.Skip(11);
+Console.WriteLine("Skip 11 elements:");
+Console.WriteLine(string.Join(", ", skipElevenElements));
--- LinqTake/Program.cs
+++ LinqTake/Program.cs
@@ -3,13 +3,13 @@
 var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 // Get first 3 elements
-var threeElements = numbers.Take(3);
+var takeThreeElements = numbers.Take(3);
 Console.WriteLine("First 3 elements:");
-Console.WriteLine(string.Join(", ", threeElements));
+Console.WriteLine(string.Join(", ", takeThreeElements));
 Console.WriteLine();
 
 // Get first 20 elements - will return only 10 elements (list of numbers contains 10 elements)
-var twentyElements = numbers.Take(20);
+var takeTwentyElements = numbers.Take(20);
 Console.WriteLine("First 20 elements:");
-Console.WriteLine(string.Join(", ", twentyElements));
+Console.WriteLine(string.Join(", ", takeTwentyElements));
 Console.WriteLine();
